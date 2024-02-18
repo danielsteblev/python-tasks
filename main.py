9a75e5ebaa65288439bb42f50e0a8eb224d1b39b
danielsteblev <groupnice@bk.ru>
2024-02-18 13:46:53 +0300

    Добавлена запись результата в файл.

--- main.py
+++ main.py
@@ -9,20 +9,17 @@ def createNewList(list1, list2):
 
     return result_list
 
-#arg1 = [2, 4, 6, 7, 11]
-#arg2 = [1, 3, 4, 2, 9, 11]
-# result_list = [6, 1, 3]
 
-arg1 = []
-arg2 = []
-f = open('input.txt', 'r')
-lines = f.readlines()
+#Чтение из файла
+input_f = open('input.txt', 'r')
+lines = input_f.readlines()
 arg1 = [int(num) for num in lines[0].split(',')]
 arg2 = [int(num) for num in lines[1].split(',')]
-print(arg1)
-print(arg2)
+input_f.close()
 
-f.close()
+#Запись в файл
+output_f = open('output.txt', 'w')
+output_f.write(str(createNewList(arg1, arg2)))
+output_f.close()
 
-print(createNewList(arg1, arg2))
 
