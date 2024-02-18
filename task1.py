a3d14551aef8ceafd9bf3191296e4a6a034ff51e
danielsteblev <groupnice@bk.ru>
2024-02-18 14:18:00 +0300

    Добавлена запись результата в файл.

--- task1.py
+++ task1.py
@@ -1,24 +1,24 @@
 def createNewList(list1, list2):
     result_list = []
-    for el1 in list1:
-        if el1 % 2 == 0 and list2.count(el1) == 0:
-            result_list.append(el1)
-    for el2 in list2:
-        if el2 % 2 == 1 and list1.count(el2) == 0:
-            result_list.append(el2)
+    for el in list1:
+        if el % 2 == 0 and list2.count(el) == 0:
+            result_list.append(el)
+    for el in list2:
+        if el % 2 == 1 and list1.count(el) == 0:
+            result_list.append(el)
 
     return result_list
 
 
 #Чтение из файла
-input_f = open('input.txt', 'r')
+input_f = open('input-files/input_task1.txt', 'r')
 lines = input_f.readlines()
 arg1 = [int(num) for num in lines[0].split(',')]
 arg2 = [int(num) for num in lines[1].split(',')]
 input_f.close()
 
 #Запись в файл
-output_f = open('output.txt', 'w')
+output_f = open('output-files/output_task1.txt', 'w')
 output_f.write(str(createNewList(arg1, arg2)))
 output_f.close()
 
